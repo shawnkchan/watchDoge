51d4e57656cf2014c8f5c5877e0bdb810955a693
Shawn Chan <shawnchan@Shawns-MacBook-Pro.local>
2023-06-29 22:46:40 +0800

    motor test

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask, request
 import requests
+from flask_cors import CORS
+from motors import rotate
 
 app = Flask(__name__)
-
+cors = CORS(app)
 
 @app.route("/")
 def hello_world():
     return "<p>HELLO WORLD</p>"
+
+@app.route("/motor")
+def run_motor():
+    rotate.rotate_motor()
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
