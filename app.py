f1c6f9e87bdb2155b389970e9e3e340f9684ee8f
Shawn Chan <shawnchan@Shawns-MacBook-Pro.local>
2023-07-08 23:42:24 +0800

    test fxn

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from flask import Flask, request, render_template
 import requests
 from flask_cors import CORS
-from motors import rotate
+from motors import rotate, test
 
 app = Flask(__name__)
 cors = CORS(app)
 
 @app.route("/")
 def hello_world():
+    test.test()
     return render_template('index.html')
 
 @app.route("/motor")
 def run_motor():
     rotate.rotate_motor()
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
