85b49ed8b3351d8940f1a53163824c9d520f8b1b
Shawn Chan <shawnchan@Shawns-MacBook-Pro.local>
2023-07-08 16:32:59 +0800

    added index.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import requests
 from flask_cors import CORS
 from motors import rotate
@@ -8,11 +8,11 @@ cors = CORS(app)
 
 @app.route("/")
 def hello_world():
-    return "<p>HELLO WORLD</p>"
+    return render_template()
 
 @app.route("/motor")
 def run_motor():
-    rotate.rotate_motor()
+    rotate.rotate_motor('index.html')
 
 
 if __name__ == '__main__':
