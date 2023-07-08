698e5f5b094a075c43580986eaf12bcc76ca52a0
Shawn Chan <shawnchan@Shawns-MacBook-Pro.local>
2023-07-08 16:12:25 +0800

    update rotate.py

--- motors/rotate.py
+++ motors/rotate.py
@@ -3,32 +3,33 @@ import time
 import sys
 from gpiozero import OutputDevice as stepper
 
+
 def rotate_motor():
     IN1 = stepper(17)
     IN2 = stepper(18)
     IN3 = stepper(27)
     IN4 = stepper(22)
-    stepPins = [IN1,IN2,IN3,IN4] # Motor GPIO pins</p><p>
+    stepPins = [IN1, IN2, IN3, IN4]  # Motor GPIO pins</p><p>
     stepDir = -1        # Set to 1 for clockwise
-                            # Set to -1 for anti-clockwise
+    # Set to -1 for anti-clockwise
     mode = 1            # mode = 1: Low Speed ==> Higher Power
-                            # mode = 0: High Speed ==> Lower Power
+    # mode = 0: High Speed ==> Lower Power
     if mode:              # Low Speed ==> High Power
-        seq = [[1,0,0,1], # Define step sequence as shown in manufacturers datasheet
-                    [1,0,0,0], 
-                    [1,1,0,0],
-                    [0,1,0,0],
-                    [0,1,1,0],
-                    [0,0,1,0],
-                    [0,0,1,1],
-                    [0,0,0,1]]
-    else:                    # High Speed ==> Low Power 
-        seq = [[1,0,0,0], # Define step sequence as shown in manufacturers datasheet
-                    [0,1,0,0],
-                    [0,0,1,0],
-                    [0,0,0,1]]
+        seq = [[1, 0, 0, 1],  # Define step sequence as shown in manufacturers datasheet
+               [1, 0, 0, 0],
+               [1, 1, 0, 0],
+               [0, 1, 0, 0],
+               [0, 1, 1, 0],
+               [0, 0, 1, 0],
+               [0, 0, 1, 1],
+               [0, 0, 0, 1]]
+    else:                    # High Speed ==> Low Power
+        seq = [[1, 0, 0, 0],  # Define step sequence as shown in manufacturers datasheet
+               [0, 1, 0, 0],
+               [0, 0, 1, 0],
+               [0, 0, 0, 1]]
     stepCount = len(seq)
-    if len(sys.argv)>1: # Read wait time from command line
+    if len(sys.argv) > 1:  # Read wait time from command line
         waitTime = int(sys.argv[1])/float(1000)
     else:
         waitTime = 0.004    # 2 miliseconds was the maximun speed got on my tests
@@ -36,9 +37,9 @@ def rotate_motor():
     stepCounter = 0
 
     while True:                          # Start main loop
-        for pin in range(0,4):
-            xPin=stepPins[pin]          # Get GPIO
-            if seq[stepCounter][pin]!=0:
+        for pin in range(0, 4):
+            xPin = stepPins[pin]          # Get GPIO
+            if seq[stepCounter][pin] != 0:
                 xPin.on()
             else:
                 xPin.off()
@@ -49,5 +50,6 @@ def rotate_motor():
             stepCounter = stepCount+stepDir
         time.sleep(waitTime)     # Wait before moving on
 
-####DELETE AFTER TESTING
+
+# DELETE AFTER TESTING
 rotate_motor()
