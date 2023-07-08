ca3c3719635933062ca8c7d9bc89117fb8ceff87
Shawn Chan <shawnchan@Shawns-MacBook-Pro.local>
2023-07-08 16:38:19 +0800

    update index.html

--- app.py
+++ app.py
@@ -15,5 +15,5 @@ def run_motor():
     rotate.rotate_motor()
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+# if __name__ == '__main__':
+#     app.run(debug=True, host='0.0.0.0', port=5000)
