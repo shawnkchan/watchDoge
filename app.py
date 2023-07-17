5ce411ad8e808f9abe6f0d2673df75589b06fb0b
Shawn Chan <shawnchan@Shawns-MacBook-Pro.local>
2023-07-18 01:07:48 +0800

    update camera

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def api_test():
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(video_stream(), mimetype='multipart/x-mixed-replace; boundary=frame$=frame')
+    return Response(video_stream(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
