0c3284631062e0857daeef68a67eecf9e653a67b
Shawn Chan <shawnchan@Shawns-MacBook-Pro.local>
2023-07-25 23:54:09 +0800

    gunicorn attempt

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ from motors import rotate, test
 import cv2
 import numpy
 from cameraCode import VideoCamera
-import threading
-
 pi_camera = VideoCamera(flip=False)
 app = Flask(__name__)
 cors = CORS(app)
@@ -49,12 +47,7 @@ def stop_video():
     response_body = {'success'}
     return jsonify(response_body)
 
-def start_camera_stream():
-    camera_thread = threading.Thread(target=gen, args=(pi_camera))
-    camera_thread.daemon = True
-    camera_thread.start()
 
 
 if __name__ == '__main__':
-    start_camera_stream()
     app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
