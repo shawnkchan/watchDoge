f3f295f3e29d21e67a7cbb5dab53b94f71b01fa5
Shawn Chan <shawnchan@Shawns-MacBook-Pro.local>
2023-08-06 02:44:06 +0800

    update

--- app.py
+++ app.py
@@ -58,4 +58,4 @@ def api_test2():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
+    app.run(debug=False, host='0.0.0.0', port=5000, threaded=True)
