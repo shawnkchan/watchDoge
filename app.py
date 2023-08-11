de3e7cd7fc21f7377cef69a2df43704b7b538dd1
Shawn Chan <shawnchan@Shawns-MacBook-Pro.local>
2023-08-11 14:17:04 +0800

    testing socketio

--- app.py
+++ app.py
@@ -44,20 +44,12 @@ def api_test():
     }
     return jsonify(response_body)
 
-# @app.route('/video_feed')
-# def video_feed():
-#     # """Video streaming route. Put this in the src attribute of an img tag."""
-#     return Response(gen(Camera()),
-#                     mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route('/video_feed')
 def video_feed():
-    cap = cv2.VideoCapture(0)  # Use the appropriate camera index
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-        _, buffer = cv2.imencode('.jpg', frame)  # Convert frame to JPEG
-        frame_data = buffer.tobytes()
-        socketio.emit('video_stream', {'data': frame_data})
+    # """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 @app.route('/api2')
 def api_test2():
